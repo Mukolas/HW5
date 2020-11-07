ac23f5cb62a376b047b193f1d2e5b7ea42200cc2
Mukolas <72994633+Mukolas@users.noreply.github.com>
2020-11-07 16:25:42 +0200

    Add files via upload

---
+++ jswork.js
@@ -0,0 +1,84 @@
+//
+//Zavdannya1 
+//
+// let dog= {
+//     name = 'Max',
+//     poroda = 'Mops',
+//     color = "white",
+//     age = 2,
+//     chystokrowna = true,
+// }
+// let human={
+//     name = 'Eva', 
+//     age = 22,
+//     rase = 'European',
+//     stat = 'wooman',
+//     bezrobitnist = false,
+// }
+// let avtomobil={
+//     marka= 'Toyota',
+//     color = 'purple',
+//     peredachi = 'auto',
+//     carAge= 4,
+//     BU = false,
+// }
+// let kwartyra={
+//     rozmir = 40,
+//     kimnati= 3,
+//     vikna = 3,
+//     tsina = 2280000,
+//     kotel = true,
+// }
+// let knyga={
+//     nazva = 'Kein Mampf :D',
+//     avtor = 'Hdolf Aitler',
+//     janr = "documental",
+//     storinok = 602,
+//     litsenziya = true,
+// }
+//
+//Zavdannya2
+//
+// let sobaka= [{name: 'Max' ,poroda: 'vivcharka'},{name: 'Rudolf' ,poroda: 'alabay'},{name: 'Swen' ,poroda: 'maops'},{name: 'Oreo' ,poroda: 'taxa'},{name: 'Elza' ,poroda: 'pudel'}]
+// console.log(sobaka);
+// let lyudyna= [{name: 'Anton' ,age: 32},{name: 'Olha' ,age: 12},{name: 'Denys' ,age: 78},{name: 'Petro' ,age: 1},{name: 'Liza' ,age: 25}]
+// console.log(lyudyna);
+// let machina= [{marka: 'Devo' ,color: 'yellow'},{marka: 'Toyota' ,color: 'red'},{marka: 'BMW' ,color:'black'},{marka :'Reno' ,color: 'purple'},{marka: 'Chewrolet',color:'blue'}]
+// console.log(machina);
+//
+//Zavdannya3
+//
+// let budynok ={
+//     rozmir=300,
+//     poverhiw=9,
+//     forma ="Kub",
+//     legalnist= true,
+//     Kwartyr = [{kimnat:3} ,{vikon:4}],
+    
+// }
+//
+//Zavdannya4
+//
+// let users = [
+//     {name: 'vasya', age: 31, status: false},
+//     {name: 'petya', age: 30, status: true},
+//     {name: 'kolya', age: 29, status: true},
+//     {name: 'olya', age: 28, status: false},
+//     {name: 'max', age: 30, status: true},
+//     {name: 'anya', age: 31, status: false},
+//     {name: 'oleg', age: 28, status: false},
+//     {name: 'andrey', age: 29, status: true},
+//     {name: 'masha', age: 30, status: true},
+//     {name: 'olya', age: 31, status: false},
+//     {name: 'max', age: 31, status: true}
+// ];
+// console.log(users[7].status);
+// console.log(users[4].status);
+// console.log(users[3].name);
+// console.log(users[6].age);
+// console.log(users[3].age);
+// console.log(users[5].name);
+// console.log(users[5].age+' '+users[5].status);
+//
+//Zavdannya5
+//
